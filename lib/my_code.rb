ff6554167ee9b87bd97f3bf203cb9ea43cf5dea9
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-03-27 15:05:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map(source_array)
+i = 0 
+negative_array = []
+while i < source_array.count do
+  negative_array << yield(source_array[i])
+  i += 1
+end
+negative_array
+end
+
+def reduce(source_array, starting_point = nil)
+  
+  if starting_point 
+memo = starting_point
+i = 0 
+else
+  memo = source_array[0]
+  i = 1 
+  end
+  while i < source_array.count do 
+     memo  = yield(memo, source_array[i])
+    i += 1 
+  end
+memo
+end
